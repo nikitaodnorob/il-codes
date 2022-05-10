a59cfd38f3ca7ac715f6508e2be8688e78ee675c
nikitaodnorob <nikitaodnorob@gmail.com>
2022-05-10 23:41:50 +0300

    Fix items of the set

--- 02_ulearn_pluralize/author3/PluralizeTask.cs
+++ 02_ulearn_pluralize/author3/PluralizeTask.cs
@@ -7,9 +7,9 @@
             count = count % 100;
             if (count % 10 == 0 || count >= 5 && count < 20 || count % 10 > 4)
                 return "рублей";
-            if (count % 10 == 1)
-                return "рубль";
-            return "рубля";
+            if (count % 10 != 1)
+                return "рубля";
+            return "рубль";
         }
 	}
 }
